1e08673256a76171b2a57ab66330eba0d2624210
3b00d09 <bd315977@gmail.com>
2023-05-10 03:06:35 +0100

    added categories rendering (they dont do anything yet)

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react"
+
+interface CategoryData{
+    id: number,
+    name: string,
+}
+
 function SideBar(){
+    const [categories ,setCategories] = useState<CategoryData[]>([])   
+    useEffect(() =>{
+
+        const fetchBooks = async()=>{
+            const response = await fetch("https://bookstore-eight-xi.vercel.app/categories")
+            const res = await response.json()
+            setCategories(res)
+        }
+        fetchBooks();
+
+    }, [])
+    
     return(
         <div className="max-w-xs bg-zinc-900 p-8 h-full">
             <h1 className="my-2">Categories</h1>
+            <ul>
+                {categories &&(
+                    categories.map((category) =>{
+                        return <li>{category.name}</li>
+                    })
+                )}
+            </ul>
         </div>
     )
 }
